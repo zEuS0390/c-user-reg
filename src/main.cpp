54a2bb63b3d8b0e5d7c55fd91f1bed24c1fc5afa
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-02-25 15:23:43 +0800

    refactor: library location and fix main code
    * set a database name
    * remove deletion of non-pointer variables
    * change SQLite library's location

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,16 @@
 #define DBFILE "dbname.txt"
 
 // Main function
-int main(int argc, char **argv) {
+int main() {
     // Declare limited entries variable for loop control
     int entries = 3;
 
     std::ifstream ifs(DBFILE);
-    std::string dbname;
+    std::string dbname = "database.db";
     std::getline(ifs, dbname);
 
     // Instantiate database connection
     DB *db = new DB(dbname.c_str());
-
-    delete &dbname;
-    delete &ifs;
 
     // Create a user table if not exist
     user::createTable(db);
@@ -38,8 +35,6 @@ int main(int argc, char **argv) {
             break;
         }
         statust status = user::register_(db, first_name.c_str(), last_name.c_str());
-        delete &first_name;
-        delete &last_name;
         entries--;
         if (status != 0) {
             break;
